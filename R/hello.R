0a243614e972eec08f9b498af45839f73c782fb4
dbolotov <dmitriy.bolotov@softwareag.com>
2019-09-17 15:01:42 -0700

    added license text to R file

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,21 @@
+# CUMULOCITYR
+#
+# Copyright (c) 2019, Software AG, Darmstadt, Germany and/or Software AG
+# USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates
+# and/or their licensors.
+#
+# This file is part of the CUMULOCITYR package for R.
+#
+# The CUMULOCITYR package is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The CUMULOCITYR package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Please see the
+# GNU General Public License for details (http://www.gnu.org/licenses/).
+# #############################################################################
 
 hello <- function() {
   print("Hello, world!")
