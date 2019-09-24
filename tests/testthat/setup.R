694ccba4940b8f34a1bbd48528261b48ecea51c8
dbolotov <dmitriy.bolotov@softwareag.com>
2019-09-24 14:19:26 -0700

    added bad credentials code to setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,11 +4,11 @@
 result_01 <- get_data(device_id = .get_cumulocity_device_id(), page_size = 2)
 result_02 <- get_data(device_id = .get_cumulocity_device_id(), page_size = 11, parse_time = FALSE)
 
-# # results used in test_utils.R
-# url_01 <- paste0(.get_cumulocity_base_url(),
-#               "/inventory/managedObjects?fragmentType=c8y_IsDevice",
-#               collapse = "")
-# result_03 <- httr::GET(url = url_01, httr::authenticate("foo","bar"))
-# cont_03 <- httr::content(result_03, "text")
-# cont_parsed_03 <- jsonlite::fromJSON(cont_03)
+# results used in test_utils.R
+url_01 <- paste0(.get_cumulocity_base_url(),
+              "/inventory/managedObjects?fragmentType=c8y_IsDevice",
+              collapse = "")
+result_03 <- httr::GET(url = url_01, httr::authenticate("foo","bar"))
+cont_03 <- httr::content(result_03, "text")
+cont_parsed_03 <- jsonlite::fromJSON(cont_03)
 
