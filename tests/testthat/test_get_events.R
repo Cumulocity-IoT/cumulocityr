541cdc7b11d3e91b842b9f2be1fbf9b0869bcba3
dbolotov <dmitriy.bolotov@softwareag.com>
2020-01-01 22:54:29 -0800

    updated tests, site

--- tests/testthat/test_get_events.R
+++ tests/testthat/test_get_events.R
@@ -1,17 +1,19 @@
 context("test get_events")
 
-test_that("page_size controls number of records returned", {
+test_that("num_rows controls number of records returned", {
   skip_on_cran()
 
   result_06 <- get_events(
-    device_id = .get_cumulocity_device_id(),
-    date_from = "2019-09-30T20:00:00Z",
+    device_id = 95637,
+    date_from = "2020-01-02T06:29:00Z",
+    date_to = "2020-01-02T06:35:10Z",
     num_rows = 2
   )
 
   result_07 <- get_events(
-    device_id = .get_cumulocity_device_id(),
-    date_from = "2019-09-30T20:00:00Z",
+    device_id = 95637,
+    date_from = "2020-01-02T06:29:00Z",
+    date_to = "2020-01-02T06:35:10Z",
     num_rows = 4
   )
 
@@ -24,8 +26,9 @@ test_that("parse_json = FALSE returns character string", {
   skip_on_cran()
 
   result_10 <- get_events(
-    device_id = .get_cumulocity_device_id(),
-    date_from = "2019-09-30T20:00:00Z",
+    device_id = 95637,
+    date_from = "2019-01-01T00:00:00Z",
+    date_to = "2021-01-01T00:00:00Z",
     num_rows = 7, parse_json = FALSE
   )
 
@@ -36,8 +39,8 @@ test_that("parse_json = FALSE returns character string", {
 test_that("when num_rows is NULL, return all records between two dates", {
   skip_on_cran()
   result_11 <- get_events(
-    device_id = .get_cumulocity_device_id(),
-    date_from = "2019-09-30T00:00:00Z", date_to = "2019-10-01T04:40:10Z"
+    device_id = 95637,
+    date_from = "2020-01-02T06:29:00Z", date_to = "2020-01-02T06:29:10Z"
   )
-  expect_equal(NROW(result_11), 1)
+  expect_equal(NROW(result_11), 2)
 })
--- tests/testthat/test_get_measurements.R
+++ tests/testthat/test_get_measurements.R
@@ -1,7 +1,7 @@
 context("test get_measurements")
 
 
-test_that("page_size controls number of records returned", {
+test_that("num_rows controls number of records returned", {
   skip_on_cran()
 
   result_01 <- get_measurements(
@@ -24,7 +24,7 @@ test_that("when num_rows is NULL, return all records between two dates", {
   skip_on_cran()
   result_10 <- get_measurements(
     device_id = .get_cumulocity_device_id(),
-    date_from = "2019-10-01T00:00:00Z", date_to = "2019-10-01T00:00:10Z"
+    date_from = "2020-01-01T00:00:00Z", date_to = "2020-01-01T00:00:10Z"
   )
   expect_equal(NROW(result_10), 2)
 })
@@ -34,7 +34,7 @@ test_that("parse_json = FALSE returns character string", {
   skip_on_cran()
   result_08 <- get_measurements(
     device_id = .get_cumulocity_device_id(),
-    date_from = "2019-09-30T20:00:00Z",
+    date_from = "2020-01-01T00:00:00Z",
     num_rows = 7, parse_json = FALSE
   )
   expect_true(inherits(result_08[[1]], "character"))
