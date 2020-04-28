4d5e26f31b5beb92171146318417c9119133f602
dbolotov <dmitriy.bolotov@softwareag.com>
2020-04-28 15:37:14 -0700

    updated date range in test

--- tests/testthat/test_get_events.R
+++ tests/testthat/test_get_events.R
@@ -7,15 +7,15 @@ test_that("num_rows controls number of records returned", {
 
   result_06 <- get_events(
     device_id = loc_device_id,
-    date_from = "2020-01-02T06:29:00Z",
-    date_to = "2020-02-02T06:35:10Z",
+    date_from = "2020-03-02T06:29:00Z",
+    date_to = "2020-03-02T06:35:10Z",
     num_rows = 2
   )
 
   result_07 <- get_events(
     device_id = loc_device_id,
-    date_from = "2020-01-02T06:29:00Z",
-    date_to = "2020-02-02T06:35:10Z",
+    date_from = "2020-03-02T06:29:00Z",
+    date_to = "2020-03-02T06:35:10Z",
     num_rows = 4
   )
 
@@ -29,8 +29,8 @@ test_that("parse_json = FALSE returns character string", {
 
   result_10 <- get_events(
     device_id = loc_device_id,
-    date_from = "2019-01-01T00:00:00Z",
-    date_to = "2021-01-01T00:00:00Z",
+    date_from = "2019-03-01T00:00:00Z",
+    date_to = "2021-03-01T00:00:00Z",
     num_rows = 7, parse_json = FALSE
   )
 
@@ -43,8 +43,8 @@ test_that("when num_rows is NULL, return all records between two dates", {
 
   result_11 <- get_events(
     device_id = loc_device_id,
-    date_from = "2020-02-02T06:29:00Z",
-    date_to = "2020-02-02T06:29:10Z"
+    date_from = "2020-03-02T06:29:00Z",
+    date_to = "2020-03-02T06:29:10Z"
   )
   expect_equal(NROW(result_11), 2)
 })
